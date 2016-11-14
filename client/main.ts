b316ada97c8d72dfd02fb6aa6796180464b6d8e8
manu <manuel.garcia@gnommo.com>
2016-11-14 14:03:12 +0100

    Angular2 WIP

---
+++ client/main.ts
@@ -0,0 +1,8 @@
+import "angular2-meteor-polyfills";
+
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+
+import { AppModule } from "./app.module";
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
